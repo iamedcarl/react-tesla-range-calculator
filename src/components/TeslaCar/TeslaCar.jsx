b8a556d24b93455154fc901e166715bc5f04aa47
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-10-06 01:10:28 -0400

    Create TeslaCar component with animated wheels

---
+++ src/components/TeslaCar/TeslaCar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TeslaCar.css';
+
+const TeslaCar = (props) => {
+  return(
+    <div className="tesla-car">
+      <div className="tesla-wheels">
+        <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
+        <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
+      </div>
+    </div>
+  );
+};
+
+// Typechecking
+TeslaCar.propTypes = {
+  wheelsize: PropTypes.number,
+};
+
+export default TeslaCar;
